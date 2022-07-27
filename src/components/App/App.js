0b592d8502876c03a213a8aed682c6c415fb287d
Allanetizen <allankosgei747@gmail.com>
2022-07-27 23:31:04 +0300

    [develop]: jsx , props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,16 @@
 import "./App.css";
-import Dashboard from "../Dashboard/Dashboard";
-import Preference from "../Preference/Preference";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Fragment } from "react";
 
-const App =()=> {
+export default function App(prop) {
+ const subject = prop.subject;
   return (
-    <div className="wrapper">
-      <h1>Application</h1>
-      <BrowserRouter>
-      <Fragment>
-        <Routes>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preference />
-          </Route>
-        </Routes>
-        </Fragment>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <p>
+          Hello, {subject}
+        </p>
+      </header>
     </div>
   );
 }
 
-export default App;
